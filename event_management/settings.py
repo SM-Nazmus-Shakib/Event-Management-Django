3926bcbbf84be7415ad3b48fc382699e0c576a84
Arnob <nzsk0990@gmail.com>
2025-07-11 22:19:52 +0600

    Add 2

--- event_management/settings.py
+++ event_management/settings.py
@@ -10,7 +10,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+
+SECRET_KEY = config('SECRET_KEY', default='0^ter^t#tt2k6gvzd3)%kw+e8!0diae_t=dd+*_^%1u9kw2k#7')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
